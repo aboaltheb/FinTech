da24c5389074cba6ca0f5a6b3b7d8b2f6c1ca18c
Moe the legend <90374941+aboaltheb@users.noreply.github.com>
2023-09-07 18:44:41 -0500

    Add files via upload

---
+++ 01_Hello_Variable_World/Add_work.py
@@ -0,0 +1,23 @@
+#dealeers increace precentage calculation
+
+
+#original car price 
+original_price = 15000.98
+
+
+#the price the deaker want to sell it for 
+current_price = 19000.99
+
+#the difference in prices will be puttin on increase 
+increase_price = current_price - original_price
+
+#the precentage for the financing 
+precent_price = (increase_price/original_price)*100
+
+#printing the prices and adding notes
+
+print(f"the original price for the car is = ${original_price}")
+print(f"the price increase = {increase_price}")
+print(f"the precentage for financing the car will be = ${precent_price} 26%")
+
+#in the application will show everything 
